dd42ed5358c741420694219557b3e547c0e09e84
BrunoPM2001 <brunoporras2001@gmail.com>
2023-06-09 09:50:50 -0500

    Evaluate different cases when rows are the same more than once

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv/config";
-import xlsx from "xlsx";
+import papa from "papaparse";
+import diacritics from "diacritics";
+import fs from "fs";
 
 const app = express();
 
@@ -10,14 +11,88 @@ app.use(express.json());
 app.use(cors());
 
 //  Main process
-app.get("/download", (req, res) => {
-  //  Read excel and convert data to json
-  const workSheet = xlsx.readFile(process.env.FIRST_EXCEL).Sheets[
-    process.env.FIRST_SHEET
-  ];
-  const excelData = xlsx.utils.sheet_to_json(workSheet);
+app.get("/importCsv", (req, res) => {
+  //  Read csv to use it
+  const csvData1 = fs.readFileSync("inputs/file1.csv", "utf8");
+  const csvData2 = fs.readFileSync("inputs/file2.csv", "utf8");
 
-  res.json({ message: "Success", data: excelData });
+  //  Convert data to json
+  const data1 = papa.parse(csvData1, {
+    header: true,
+    delimiter: ",",
+    skipEmptyLines: true,
+  }).data;
+  const data2 = papa.parse(csvData2, {
+    header: true,
+    delimiter: ",",
+    skipEmptyLines: true,
+  }).data;
+
+  let res1 = 0;
+  let res2 = 0;
+  let finde_it = 0;
+  let notFound = [];
+  let moreThanOnce = [];
+  let matchRow = 0;
+
+  //  Search in all rows of data 2
+  for (let i = 0; i < data2.length; i++) {
+    for (let j = 0; j < data1.length; j++) {
+      //  If names and grade matches do somethings
+      if (
+        diacritics.remove(data2[i].ape_paterno).trim() ==
+          diacritics.remove(data1[j].ape_paterno).trim() &&
+        diacritics.remove(data2[i].ape_materno).trim() ==
+          diacritics.remove(data1[j].ape_materno).trim() &&
+        diacritics.remove(data2[i].nom_alumno).trim() ==
+          diacritics.remove(data1[j].nom_alumno).trim() &&
+        data2[i]["M/D"].trim() == data1[j]["M/D"].trim()
+      ) {
+        //  Save values of the n° of row where its equals
+        matchRow = j;
+        res2++; //  Count of equals rows
+        finde_it++; //  How many times data2's row is in data1
+      }
+    }
+    //  If it doesn't match with any row of data1
+    if (finde_it == 0) {
+      notFound.push({
+        ape_paterno: data2[i]["ape_paterno"],
+        ape_materno: data2[i]["ape_materno"],
+        nombres: data2[i]["nom_alumno"],
+      });
+    }
+    //  If it was more than once in data1
+    else if (finde_it > 1) {
+      moreThanOnce.push({
+        ape_paterno: data2[i]["ape_paterno"],
+        ape_materno: data2[i]["ape_materno"],
+        nombres: data2[i]["nom_alumno"],
+        how_many_times: finde_it,
+      });
+    }
+    //  If it's just one coincidence
+    else if (finde_it == 1) {
+      //  Save all empty cells in data1 - TO DO
+      data1[matchRow] = data2[i];
+    }
+    finde_it = 0;
+  }
+
+  //  Create csv with data1 in result path
+  const newCsv = papa.unparse(data1);
+  fs.writeFileSync("result/output.csv", newCsv, "utf-8");
+
+  //  Response in JSON
+  res.json({
+    message: "Success",
+    coincidencia_nombres: res1,
+    coincidencia_nombres_y_grado: res2,
+    cuenta_inicial: data2.length,
+    notFound,
+    moreThanOnce,
+  });
 });
 
-app.listen(3000);
+//  Launch app
+app.listen(3000, () => console.log("Listening on port 3000"));
